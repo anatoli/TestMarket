6131531718e6d10a42989cc08299cd037ec35541
Anatoliy Arinovich <arinov@tut.by>
2016-12-05 17:04:04 +0300

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testMarketApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the testMarketApp
+ */
+angular.module('testMarketApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
